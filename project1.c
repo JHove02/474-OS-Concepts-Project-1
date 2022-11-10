7d314219a068536cc93b71595869feb1d2ad5900
JHove <jordan.n.hove@ndsu.edu>
2022-11-10 13:24:15 -0600

    Finished code

--- project1.c
+++ project1.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <stdbool.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdbool.h>
 
 int main()
 {
 
     bool loop = true;
-    int process;
-   
+    int processes;
+
     int fileLength = 0;
-    
 
     while (loop)
     {
 
-        // input for # of processes
+        // input for # of processese
         printf("Enter the number 1, 2, or 4 for the number of processes to run\n");
 
-        scanf("%d", &process);
-        if (process == 1 || process == 2 || process == 4)
+        scanf("%d", &processes);
+        if (processes == 1 || processes == 2 || processes == 4)
         {
             loop = false;
         }
         else
         {
-            printf("\n%d is not a valid process\n", process);
+            printf("\n%d is not a valid processes\n", processes);
         }
     }
 
@@ -53,21 +54,28 @@ int main()
 
     int finalNum = 0; // final sum of the file
 
-    //loop for all processes
-    for (int i = 0; i < process; i++)
+    int fd[processes][2];
+
+    struct timeval current_time;
+    gettimeofday(&current_time, NULL);
+    suseconds_t timer = current_time.tv_usec;
+    // loop for all processes
+    for (int i = 0; i < processes; i++)
     {
-        int fd[2];
-        if(pipe(fd)==-1){ //check if pipe was made
-                return -1;
-        }
-        if (fork() == 0) //if not parent process
+
+        if (pipe(fd[i]) == -1)
         {
-            
-             FILE *fp;// open the file
+            printf("Pipe failed to create");
+            return -1;
+        }
+        if (fork() == 0) // if not parent processes
+        {
+
+            FILE *fp; // open the file
             switch (file)
             {
             case 1:
-                fp = fopen("file1.dat", "r"); //open file read only
+                fp = fopen("file1.dat", "r"); // open file read only
                 fileLength = 1000;
                 break;
             case 2:
@@ -79,21 +87,21 @@ int main()
                 fileLength = 100000;
                 break;
             }
-            
-            if(fp == NULL){ //if file not found
+
+            if (fp == NULL)
+            { // if file not found
                 printf("File not located\n");
                 return -1;
             }
 
-            int offset = (i / (float)process) * fileLength * 5; // Size of the area each child must read
-            printf("blocksize, %d\n", offset);
-            fseek(fp, offset, SEEK_SET);   // jump to an offset in the file so the child can scan that section
-            
-            int blockTotal = 0;                     // Sum of the block each process reads
-            for (int j = 0; j < (fileLength/process); j++) // scan each line within the childs designated index
+            int offset = (i / (float)processes) * fileLength * 5; // Size of the area each child must read
+            fseek(fp, offset, SEEK_SET); // jump to an offset in the file so the child can scan that section
+
+            int blockTotal = 0;                              // Sum of the block each processes reads
+            for (int j = 0; j < (fileLength / processes); j++) // scan each line within the childs designated index
             {
                 int num;
-                if (fscanf(fp, "%d", &num)==1) // scan line and add num to blockTotal
+                if (fscanf(fp, "%d", &num) == 1) // scan line and add num to blockTotal
                 {
                     blockTotal += num;
                 }
@@ -103,24 +111,30 @@ int main()
                 }
             }
 
-            //close(fd[0]);
-            write(fd[1], &blockTotal, sizeof(int));
+            // close(fd[0]);
+            write(fd[i][1], &blockTotal, sizeof(int));
             fclose(fp);
             exit(0);
         }
-        else{
-            //close(fd[1]);
-            int childTotal;
-            read(fd[0], &childTotal, sizeof(int));
-            //close(fd[0]);
-            printf("\nChild %d sum is: %d", i, childTotal);
-            
-            finalNum += childTotal;
-            printf("\nFinal num: %d\n", finalNum);
-        }
     }
 
+    wait(NULL); //wait for child processes
+    //read child process data
+    for(int i =0; i < processes; i++){
+        // close(fd[1]);
+        int childTotal;
+        read(fd[i][0], &childTotal, sizeof(int));
+        // close(fd[0]);
+        printf("\nChild %d sum is: %d", i, childTotal);
+
+        finalNum += childTotal;
+    }
     printf("\nTotal sum is: %d", finalNum);
+
+    gettimeofday(&current_time, NULL);
+    timer = current_time.tv_usec - timer;
+
+    printf("\nTime taken (microseconds): %ld", timer);
 
     return 0;
 }
